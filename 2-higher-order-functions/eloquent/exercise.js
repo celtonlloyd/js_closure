076742ec7d90525953254a163236b19382931f01
Elton <celtonlloyd@gmail.com>
2020-10-13 02:13:44 +0530

    completed eloquent ex

--- 2-higher-order-functions/eloquent/exercise.js
+++ 2-higher-order-functions/eloquent/exercise.js
@@ -1,35 +1,57 @@
 // Challenge 1. Flattening
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
+arrays.reduce((a, c) => {
+  a.concat(c);
+  return a;
+}, []);
+
 // Your code here.
 // → [1, 2, 3, 4, 5, 6]
 
 // Challenge 2. Your own loop
 // Your code here.
 
-loop(3, n => n > 0, n => n - 1, console.log);
+function loop(value, test, update, body) {
+  while (test(value)) {
+    body(value);
+    value = update(value);
+  }
+}
+
+loop(
+  3,
+  (n) => n > 0,
+  (n) => n - 1,
+  console.log
+);
 // → 3
 // → 2
 // → 1
 
 // Challenge 3. Everything
+
+// using some
+
 function every(array, test) {
-  // Your code here.
+  return array.reduce((a, c) => a && [c].some((e) => test(e)), true);
 }
 
-console.log(every([1, 3, 5], n => n < 10));
+// using loop
+
+function every2(array, test) {
+  let output = true;
+  for (let num of array) {
+    output = output && test(num);
+  }
+  return output;
+}
+
+console.log(every([1, 3, 5], (n) => n < 10));
 // → true
-console.log(every([2, 4, 16], n => n < 10));
+console.log(every([2, 4, 16], (n) => n < 10));
 // → false
-console.log(every([], n => n < 10));
+console.log(every([], (n) => n < 10));
 // → true
 
-// Challenge 4. Dominant writing direction
-function dominantDirection(text) {
-  // Your code here.
-}
 
-console.log(dominantDirection("Hello!"));
-// → ltr
-console.log(dominantDirection("Hey, مساء الخير"));
-// → rtl
